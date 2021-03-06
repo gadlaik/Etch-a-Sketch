48fce76fc36146e6f8ed07d9c2df1b11d3522143
Mensur K <sabatonz@hotmail.com>
2021-03-06 23:19:42 +0100

    Project completed

--- script.js
+++ script.js
@@ -14,19 +14,28 @@ container.addEventListener("mouseover", (e) => {
   console.log(e.target);
 });
 
-//clear
+// new
 clear.addEventListener("click", (e) => {
-  document.querySelectorAll(".square").forEach((square) => {
-    square.classList.remove("draw");
-  });
+  //   document.querySelectorAll(".square").forEach((square) => {
+  //     square.classList.remove("draw");
+  //   });
 
-  //   let userInput = prompt("How many H/V squares do you want: ");
-  //   if (userInput > 100) {
-  //     alert("Maximum number of squares H/V is a 100");
-  //     return;
-  //   }
-  //   container.setAttribute(
-  //     "style",
-  //     `grid-template-rows: repeat(${userInput}, auto); grid-template-columns: repeat(${userInput}, auto)`
-  //   );
+  let userInput = prompt("How many H/V squares do you want: ");
+  if (userInput > 100) {
+    alert("Maximum number of squares H/V is a 100");
+    return;
+  }
+  container.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${userInput}, 1fr);`
+  );
+
+  let squareTotal =
+    userInput * userInput -
+    Array.from(document.querySelectorAll(".square")).length;
+  for (let i = 1; i <= squareTotal; ++i) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
+  }
 });
